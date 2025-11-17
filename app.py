a53425403b5d92765541dc641ce2dac525f813da
Ilyas Sülen <ilyassulen@Mac.local>
2025-11-17 16:01:13 +0100

    Setup SQLAlchemy and database initialization

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from models import db, User, Subscription
+import os
+
 
 app = Flask(__name__)
+
+# Configuring SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/subs.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Connect DB & App
+db.init_app(app)
 
 
 @app.route('/')
@@ -9,4 +20,6 @@ def index():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
